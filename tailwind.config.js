b34cc7f2dddc2ede75f93609ac23fbb8f7dae0a6
PedroMontolive <pedro.montolive@gmail.com>
2023-05-18 15:01:22 -0300

    remap

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App/Views/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
