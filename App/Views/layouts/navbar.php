4e9b7611d065674f9de5d85f330af3fa0f902619
PedroMontolive <pedro.montolive@gmail.br>
2023-05-28 22:14:17 -0300

    image click

--- App/Views/layouts/navbar.php
+++ App/Views/layouts/navbar.php
@@ -1,7 +1,7 @@
 <div class="section font-semibold px-16 text-gray-800 fixed w-full top-0 flex header_section bg-red-500 shadow z-10">
   <div class="sub_head flex my-auto py-3">
-    <div class="logo w-14"><img class="w-full" src="<?php echo 'http://'.APP_HOST.'/public/images/pizza-logo.png'; ?>" alt="" /></div>
-    <div class="text ml-2 my-auto flex-none">Pizzaria La Bilonheza</div>
+    <a href="<?php echo 'http://'.APP_HOST.'/'; ?>"><div class="logo w-14"><img class="w-full" src="<?php echo 'http://'.APP_HOST.'/public/images/pizza-logo.png'; ?>" alt="" /></div></a>
+    <div class="text ml-2 my-auto flex-none"><a href="<?php echo 'http://'.APP_HOST.'/'; ?>">Pizzaria La Bilonheza</a></div>
   </div>
   <div class="sub_head ml-auto flex space-x-8 my-auto">
     <div class="h_btns cursor-pointer hover:text-gray-100"><a href="<?php echo 'http://'.APP_HOST.'/'; ?>">Home</a></div>
